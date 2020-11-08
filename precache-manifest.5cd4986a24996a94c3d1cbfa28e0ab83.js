4f78767d1d1c37f3298f67968824955598c68dbc
liumiao <liu1127miao@qq.com>
2020-11-08 17:36:20 +0800

    11-8

---
+++ precache-manifest.5cd4986a24996a94c3d1cbfa28e0ab83.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b74fd78c72a777aa64237444daf4bd6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0bde61c2870feb4a9d43",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "43c0a12a23616b6c58c2",
+    "url": "/static/css/main.c20fd361.chunk.css"
+  },
+  {
+    "revision": "0bde61c2870feb4a9d43",
+    "url": "/static/js/2.c7bf2721.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.c7bf2721.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43c0a12a23616b6c58c2",
+    "url": "/static/js/main.e6d982c3.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
